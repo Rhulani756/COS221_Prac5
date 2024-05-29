587982490fffa7f372a219a130f7332322ca4272
Andile Mahlaba <mahlabaandile59@gmail.com>
2024-05-30 01:42:32 +0200

    Update tvseries.php
    CSS changes again

--- tvseries.php
+++ tvseries.php
@@ -8,6 +8,7 @@ session_start();
 <head>
     <title>View</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="StyleLooks2.css">
     <style>
         .alb {
             width: 100%;
